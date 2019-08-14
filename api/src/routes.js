818d1d1e1277eecc4b1eeaa6b07297c91f0e66f5
Fabricio <fabricioobedin@gmail.com>
2019-08-13 22:19:01 -0300

    adding consumer and producer

--- api/src/routes.js
+++ api/src/routes.js
@@ -2,8 +2,16 @@ import express from "express";
 const routes = express.Router();
 
 routes.post("/certificates", async (req, res) => {
-  //TODO: Call microservice
-  console.log(req.producer);
+  const message = {
+    user: { id: 1, name: "Fabricio Bedin" },
+    course: "Kafka com Node.js",
+    grade: 10
+  };
+
+  await req.producer.send({
+    topic: "issue-certificate",
+    messages: [{ value: JSON.stringify(message) }]
+  });
 
   return res.json({
     ok: true
